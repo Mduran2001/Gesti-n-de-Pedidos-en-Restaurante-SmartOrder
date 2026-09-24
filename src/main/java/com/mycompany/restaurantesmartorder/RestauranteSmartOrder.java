d0b7051de98f011bc46a6e014778006b2ff50d29
Mario <duranbmario@gmail.com>
2026-09-23 18:42:08 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/restaurantesmartorder/RestauranteSmartOrder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantesmartorder;
+
+/**
+ *
+ * @author mario
+ */
+public class RestauranteSmartOrder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
